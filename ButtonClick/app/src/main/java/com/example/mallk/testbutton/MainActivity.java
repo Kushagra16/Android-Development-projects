e8bf67c0084f895d0d0a0d8a82b84c48dfa27f52
studentuser <studentuser@studentusers-MacBook-Pro.local>
2017-01-24 23:15:37 -0800

    Adding new files

---
+++ ButtonClick/app/src/main/java/com/example/mallk/testbutton/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mallk.testbutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                count++;
+                button.setText("You Clicked me "+ count + " times");
+            }
+        });
+
+    }
+}
