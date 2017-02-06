b2e28c1f1604d5f3a0bb86b0bffd2dc96093e0c5
studentuser <studentuser@studentusers-MacBook-Pro.local>
2017-02-06 11:35:49 -0800

    New Project

---
+++ Activities/app/src/main/java/com/example/mallk/week5_intent/Main2Activity.java
@@ -0,0 +1,20 @@
+package com.example.mallk.week5_intent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent i = getIntent();
+        String message = i.getStringExtra(MainActivity.MESSAGE);
+        TextView tv = (TextView)findViewById(R.id.textView);
+        tv.setText(message);
+    }
+}
